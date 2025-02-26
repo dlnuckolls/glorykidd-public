6a9b1852f97986e7f677ae81e33a93d532c360fb
David Nuckolls <dlnuckolls@gmail.com>
2025-02-26 12:01:00 -0500

    Reset web building location for GKWeb

--- web/gkquotes/gkquotes/Data/ApplicationDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace gkquotes.Data {
-  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): IdentityDbContext<ApplicationUser>(options) {
-  }
-}
