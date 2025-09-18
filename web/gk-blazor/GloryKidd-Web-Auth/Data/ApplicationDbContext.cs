7e4279338b74ea77598f939216cfc8b99ab09461
David Nuckolls <dlnuckolls@gmail.com>
2025-09-18 10:58:39 -0400

    removing old project

--- web/gk-blazor/GloryKidd-Web-Auth/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace GloryKidd_Web_Auth.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-{
-}
