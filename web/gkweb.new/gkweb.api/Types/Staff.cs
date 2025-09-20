bdeaa9165feb5319828eee2dca8afed22e50a1b1
David Nuckolls <dlnuckolls@gmail.com>
2025-09-20 10:59:24 -0400

    Add new solution for GKWeb rewrite

---
+++ web/gkweb.new/gkweb.api/Types/Staff.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace gkweb.api.Types;
+
+public class Staff
+{
+
+  public Staff() { }
+
+  public string? Group { get; set; }
+  public string? Title { get; set; }
+  public string? Summary { get; set; }
+  public string? imageUrl { get; set; }
+}
