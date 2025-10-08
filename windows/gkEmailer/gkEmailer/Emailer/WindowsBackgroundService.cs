5bd7c11b8f8d618a6547233ed527fd33c844bf6a
David Nuckolls <dlnuckolls@gmail.com>
2025-10-08 13:54:35 -0400

    updates

--- windows/gkEmailer/gkEmailer/Emailer/WindowsBackgroundService.cs
+++ windows/gkEmailer/gkEmailer/Emailer/WindowsBackgroundService.cs
@@ -1,20 +1,28 @@
 namespace Emailer;
 
-public sealed class WindowsBackgroundService (
+public sealed class WindowsBackgroundService(
     JokeService jokeService,
-    ILogger<WindowsBackgroundService> logger) : BackgroundService {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
-        try {
-            while (!stoppingToken.IsCancellationRequested) {
+    ILogger<WindowsBackgroundService> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
                 string joke = jokeService.GetJoke();
                 logger.LogWarning("{Joke}", joke);
 
                 await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
             }
-        } catch (OperationCanceledException) {
+        }
+        catch (OperationCanceledException)
+        {
             // When the stopping token is canceled, for example, a call made from services.msc,
             // we shouldn't exit with a non-zero exit code. In other words, this is expected...
-        } catch (Exception ex) {
+        }
+        catch (Exception ex)
+        {
             logger.LogError(ex, "{Message}", ex.Message);
 
             // Terminates this process and returns an exit code to the operating system.
